6a063eb911689a5979a84caf9ad203399ea11a1c
bibhuti <bhusan.bibhutimohapatra@gmail.com>
2024-07-02 11:52:50 +0530

    Sample DataFrame creation

---
+++ src/main/scala/org/basics/DataFrameJSONSource.scala
@@ -0,0 +1,25 @@
+package org.basics
+
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.types.{IntegerType, StringType, StructField, StructType}
+
+object DataFrameJSONSource {
+  def main(args: Array[String]): Unit = {
+    val spark = SparkSession
+      .builder()
+      .master("local[1]")
+      .appName("SparkDemo")
+      .getOrCreate()
+
+    import spark.implicits._
+
+    val mySchema = StructType(
+      List(
+        StructField("name",StringType)
+        ,StructField("age",IntegerType)
+      )
+    )
+    val df = spark.read.format("json").schema(mySchema).load("src/main/resources/datasets/people.json")
+    df.show()
+  }
+}
