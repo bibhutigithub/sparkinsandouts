72563da040dcc6223fb5d22c8b7fc77a022bb7e1
bibhuti <bhusan.bibhutimohapatra@gmail.com>
2024-07-03 15:27:34 +0530

    Complex Nested JSON demo

--- src/main/scala/org/basics/ComplexJSONParsing.scala
+++ src/main/scala/org/basics/ComplexJSONParsing.scala
@@ -2,6 +2,7 @@ package org.basics
 
 import org.apache.spark.sql.types._
 import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.functions.{col, explode}
 
 object ComplexJSONParsing {
   def main(args: Array[String]): Unit = {
@@ -12,5 +13,50 @@ object ComplexJSONParsing {
       .getOrCreate()
 
     import spark.implicits._
+    val mySchema = StructType(
+      List(
+        StructField("id",StringType)
+        ,StructField("type",StringType)
+        ,StructField("name",StringType)
+        ,StructField("ppu",DoubleType)
+        ,StructField("batters",StructType(
+          List(
+            StructField("batter",ArrayType(
+              StructType(
+                List(
+                   StructField("id",StringType)
+                  ,StructField("type",StringType)
+                )
+              )
+            ))
+          )
+        ))
+        ,StructField("topping",ArrayType(
+          StructType(
+            List(
+              StructField("id",StringType)
+              ,StructField("type",StringType)
+            )
+        ))
+      )
+    ))
+    val df = spark.read
+      .format("json")
+      .option("multiLine",true)
+      .schema(mySchema)
+      .load("src/main/resources/datasets/donut_nested.json")
+
+    val result_df = df.select("*","batters.*")
+                    .drop("batters")
+                    .withColumn("batter",explode(col("batter")))
+                    .withColumn("batter_id",col("batter.id"))
+                    .withColumn("batter_type",col("batter.type"))
+                    .drop("batter")
+                    .withColumn("topping",explode(col("topping")))
+                    .withColumn("topping_id",col("topping.id"))
+                    .withColumn("topping_type",col("topping.type"))
+                    .drop("topping")
+
+    print(result_df.count())
   }
 }
