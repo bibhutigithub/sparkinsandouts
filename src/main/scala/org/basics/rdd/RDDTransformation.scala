155d3bd28471f2982771b67da1ee10671ae8c39a
bibhuti <bhusan.bibhutimohapatra@gmail.com>
2024-08-17 08:34:02 +0530

    RDD Transformations word counts

---
+++ src/main/scala/org/basics/rdd/RDDTransformation.scala
@@ -0,0 +1,19 @@
+package org.basics.rdd
+
+import org.apache.spark.sql.SparkSession
+
+object RDDTransformation {
+  def main(args: Array[String]): Unit = {
+    val spark = SparkSession
+      .builder()
+      .master("local[1]")
+      .appName("SparkDemo")
+      .getOrCreate()
+
+    val wordRdd = spark.sparkContext.textFile("src/main/resources/datasets/word_counts.txt")
+    val resultRdd = wordRdd.flatMap(line => line.split(" "))
+      .map(word => (word, 1))
+      .reduceByKey(_+_)
+    resultRdd.collect.foreach(println)
+  }
+}
